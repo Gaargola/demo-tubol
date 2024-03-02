e997a77d56289ae8dd7836d9d7a478c69fdf0a8f
Mauro Borda <88571889+Gaargola@users.noreply.github.com>
2024-03-02 10:31:59 -0300

    estructure basic html

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Solutions</a>
+        </li>
+        <li>
+          <a href="#">Tribe</a>
+        </li>
+        <li>
+          <a href="#">Insights</a>
+        </li>
+        <li>
+          <a href="#">Why Tubol</a>
+        </li>
+        <li>
+          <a href="#">LOG IN</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
